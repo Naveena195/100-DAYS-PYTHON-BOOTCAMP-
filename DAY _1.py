12fed9dd519d32259227277c2f8f5860cb854fa2
Naveena Runku <kdwani1114@gmail.com>
2025-02-14 17:10:22 +0530

    Update DAY _1.py

---
+++ DAY _1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band name Generator.\n")
+cityname=input("What's the name of the city you grew in?\n")
+
+petname=input("What's your pet's name?\n" )
+print("your band name to be "+cityname+" " +petname)
